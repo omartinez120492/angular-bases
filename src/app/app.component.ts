f5974e1a963514b3a0379bf1d93ddb38883fee85
omartinez120492 <omar1204@gmail.com>
2024-10-09 08:48:34 -0600

    decorador input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavItem } from './interfaces/components.interface';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   
   public nombre: string = 'omar martínez';
-
+  
+  public opciones: NavItem[] = [
+    { name: 'Productos', route: '/productos' },
+    { name: 'Usuarios', route: '/users' },
+    { name: 'Plataforma', route: '/platform' },
+    { name: 'Reportes', route: '/reports' },
+    { name: 'Configuracion', route: '/config' },
+    { name: 'Cerrar sesion', route: '/logout' },
+    { name: 'Cambiar contraseña', route: '/change-password' },
+  ];
 
 }
--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NavItem } from '../interfaces/components.interface';
 
 @Component({
   selector: 'app-sidenav',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class SidenavComponent {
+  @Input()
+  public componentes: NavItem[] = [];
 
+  
 }
